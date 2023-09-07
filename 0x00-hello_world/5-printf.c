441bdb1cff05344c98a38600ef7311a05c2b7e0a
Cryptonfused <cryptonfused@gmail.com>
2023-09-07 09:55:13 -0700

    code for c program tha prints "gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out"

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - the main funct.
+ * puts - the put funct.
+ *
+ * Return: 0 if all goes well and 1 otherwise.
+ */
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
