5524dc041467135345f4185b8d4982812bd3ddda
Cryptonfused <cryptonfused@gmail.com>
2023-09-07 09:47:19 -0700

    fix to c program code to display exactly as intended with "

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  *
  * Return: 0 if all goes well and 1 otherwise.
  */
-int main()
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
