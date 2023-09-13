d44aad2d0245e43f7d02a7699fa419ea6e392747
Cryptonfused <cryptonfused@gmail.com>
2023-09-13 06:20:01 -0700

    Mod to code of last digit funct

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
- * print_last_digit - Prints the last digit of a number
- * @n: The int to store value entered for last digit extraction
- * Return: value of the last digit
+ * print_last_digit - Prints the last digit of a num.
+ * @n: The int var to store num entered for extraction
+ * of last digit.
+ * 
+ * Return: last digit
  */
 int print_last_digit(int n)
 {
 	int l;
 
-	if (n < 0)
-		n = -n;
 	l = n % 10;
+	if(l < 0)
+		l = -l;
 	_putchar(l + '0');
 	return (l);
-
 }
