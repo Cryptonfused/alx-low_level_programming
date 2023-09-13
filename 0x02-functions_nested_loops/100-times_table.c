aec9a2dac52d32f657ab74a22d4c60f633e736f5
Cryptonfused <cryptonfused@gmail.com>
2023-09-13 10:07:07 -0700

    test

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,50 @@
 #include "main.h"
 
-/**
- * print_times_table - Prints the n times table, starting from 0
- *does not print for n less than 0 or greater than 15
- * @n: the number to print
- */
-void print_times_table(int n)
-{
-	int i, j, k;
 
-	if (n >= 0 && n <= 15)
+	/**
+	 * print_times_table - prints the n times table, starting with 0
+	 * @n: number of the times table
+	 */
+	void print_times_table(int n)
 	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				k = j * i;
+		int i, j, k;
 
-				if (j == 0)
+
+		if (n >= 0 && n <= 15)
+		{
+			for (i = 0; i <= n; i++)
+			{
+				for (j = 0; j <= n; j++)
 				{
-					_putchar('0' + k);
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					if (k < 10)
+					k = j * i;
+					if (j == 0)
 					{
+						_putchar(k + '0');
+					} else if (k < 10 && j != 0)
+					{
+						_putchar(',');
 						_putchar(' ');
 						_putchar(' ');
-						_putchar('0' + k);
+						_putchar(' ');
+						_putchar(k + '0');
+					} else if (k >= 10 && k < 100)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar((k / 10) + '0');
+						_putchar((k % 10) + '0');
+					} else if (k >= 100)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar((k / 100) + '0');
+						_putchar(((k / 10) % 10) + '0');
+						_putchar((k % 10) + '0');
 					}
-					else if (k < 100)
-					{
-						_putchar(' ');
-						_putchar('0' + (k / 10));
-						_putchar('0' + (k % 10));
-					}
-					else
-					{
-						_putchar('0' + (k / 100));
-						_putchar('0' + ((k % 100) / 10));
-						_putchar('0' + (k % 10));
 				}
+				_putchar('\n');
 			}
-			_putchar('\n');
 		}
 	}
-	_putchar('\n');
-}
+
