8b6b98341af669a48a8bb30f5b1e712171ba4f9f
Cryptonfused <cryptonfused@gmail.com>
2023-09-13 06:23:09 -0700

    mod to adhere to betty guide

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
  * print_last_digit - Prints the last digit of a num.
  * @n: The int var to store num entered for extraction
  * of last digit.
- * 
+ *
  * Return: last digit
  */
 int print_last_digit(int n)
@@ -11,7 +11,7 @@ int print_last_digit(int n)
 	int l;
 
 	l = n % 10;
-	if(l < 0)
+	if (l < 0)
 		l = -l;
 	_putchar(l + '0');
 	return (l);
