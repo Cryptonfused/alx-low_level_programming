3dc3d7e803aed28fa0a7c4ed8c42b1f3b0aef79e
Cryptonfused <cryptonfused@gmail.com>
2023-09-13 08:17:20 -0700

    funct that return sum of two nums

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns the result
+ * @a: Integer to be added to b
+ * @b: Integer to be added to a
+ *
+ * Return: Sum of a and b
+ */
+int add(int a, int b)
+{
+
+	return (a + b);
+}
