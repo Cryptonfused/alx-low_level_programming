157cf06212f5ec5d9354c00a72fcd04e81682109
Cryptonfused <cryptonfused@gmail.com>
2023-09-15 05:05:36 -0700

    new code for program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,72 +1,45 @@
 #include <stdio.h>
 
 /**
- * multiply - Multiplies two matrices
- * @F: First matrix
- * @M: Second matrix
- */
-void multiply(int F[2][2], int M[2][2])
-{
-	int x = F[0][0] * M[0][0] + F[0][1] * M[1][0];
-	int y = F[0][0] * M[0][1] + F[0][1] * M[1][1];
-	int z = F[1][0] * M[0][0] + F[1][1] * M[1][0];
-	int w = F[1][0] * M[0][1] + F[1][1] * M[1][1];
-
-	F[0][0] = x;
-	F[0][1] = y;
-	F[1][0] = z;
-	F[1][1] = w;
-}
-
-/**
- * power - Raises matrix F to power n
- * @F: Matrix to be raised
- * @n: Exponent
- */
-void power(int F[2][2], int n)
-{
-	if (n <= 1)
-		return;
-
-	int M[2][2] = {{1, 1}, {1, 0}};
-	power(F, n / 2);
-	multiply(F, F);
-
-	if (n % 2 != 0)
-		multiply(F, M);
-}
-
-/**
- * fib - Calculates Fibonacci number at position n
- * @n: Position
- * Return: Fibonacci number
- */
-int fib(int n)
-{
-	if (n <= 0)
-		return 0;
-
-	int F[2][2] = {{1, 1}, {1, 0}};
-	power(F, n - 1);
-
-	return F[0][0];
-}
-
-/**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main - Fibonacci sequence
+ *
+ * Return: Always 0
  */
 int main(void)
 {
-	int n = 98;
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsigned long int aft2;
 
-	for (int i = 0; i < n; i++)
+	printf("%lu", bef);
+
+	for (i = 1; i < 91; i++)
 	{
-		printf("%d", fib(i + 1));
-		if (i < n - 1)
-			printf(", ");
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
 	}
 
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
+	}
 	printf("\n");
 	return (0);
 }
+
