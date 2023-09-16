ef96c857f9fcac7b218be376772d23f916a71d78
Cryptonfused <cryptonfused@gmail.com>
2023-09-16 05:20:29 -0700

    code to print int

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,17 +6,39 @@
  */
 void print_number(int n)
 {
+	int is_negative = 0;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
+		is_negative = 1;
 	}
 
-	if (n / 10)
+	int reversed_number = 0;
+
+	while (n > 0)
 	{
-		print_number(n / 10);
+		reversed_number = reversed_number * 10 + n % 10;
+		n /= 10;
 	}
 
-	_putchar((n % 10) + '0');
+	if (reversed_number == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		while (reversed_number > 0)
+		{
+			_putchar((reversed_number % 10) + '0');
+			reversed_number /= 10;
+		}
+	}
+
+	if (is_negative)
+	{
+		_putchar('-');
+	}
 }
 
