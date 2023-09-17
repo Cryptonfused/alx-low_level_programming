8374a196e401269159457ef7c3dfb4a898911790
Cryptonfused <cryptonfused@gmail.com>
2023-09-17 01:23:52 -0700

    new code for function that prints an int - uses unsigned int in an attempt to pass check12.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,39 +1,19 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * print_number - Prints an integer.
- *
- * @n: The integer to be printed.
+ * print_number - prints an interger using _putchar.
+ * @n: the integer to be printed.
  */
 void print_number(int n)
 {
-	int l, c = 1;
+	unsigned int num = n;
 
 	if (n < 0)
+
 	{
 		_putchar('-');
-		n = -n;
+		num = -num;
 	}
-
-	if (n == 0)
-		_putchar('0');
-	else if (n < 10)
-		_putchar(n + '0');
-	else
-	{
-		l = n;
-		while (l / 10 > 0)
-		{
-			c *= 10;
-			l /= 10;
-		}
-
-		while (c > 0)
-		{
-			_putchar((n / c) + '0');
-			n %= c;
-			c /= 10;
-		}
-	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
 }
