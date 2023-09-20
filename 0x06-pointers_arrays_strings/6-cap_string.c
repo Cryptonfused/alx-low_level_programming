e5b4404d7753fa53376c280cb672a09fa58a8d86
Cryptonfused <cryptonfused@gmail.com>
2023-09-20 16:39:08 -0700

    code for funct that capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * cap_string - Capitalizes the first letter of each word in a string.
+ * @str: Pointer to the input string.
+ *
+ * Return: Pointer to the modified string.
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 || (str[i - 1] == ' ' || str[i - 1] == '\t' ||
+			       str[i - 1] == '\n' || str[i - 1] == ',' ||
+			       str[i - 1] == ';' || str[i - 1] == '.' ||
+			       str[i - 1] == '!' || str[i - 1] == '?' ||
+			       str[i - 1] == '"' || str[i - 1] == '(' ||
+			       str[i - 1] == ')' || str[i - 1] == '{' ||
+			       str[i - 1] == '}') && (str[i] >= 'a' && str[i] <= 'z'))
+		{
+			str[i] -= 32; /* Convert to uppercase by subtracting 32 */
+		}
+	}
+
+	return (str);
+}
