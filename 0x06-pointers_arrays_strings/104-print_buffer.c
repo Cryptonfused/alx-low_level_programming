eb62a69da6667dc981b5aab682b4f3e18021332a
Cryptonfused <cryptonfused@gmail.com>
2023-09-20 19:48:20 -0700

    attempt fix

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -3,48 +3,44 @@
 
 /**
  * print_buffer - Prints the content of a buffer.
- * @b: The buffer to print.
- * @size: The number of bytes to print.
- *
- * Description: This function prints the content of the buffer in the specified
- * format, including hexadecimal representation and printable characters.
+ * @b: The buffer to be printed.
+ * @size: The size of the buffer.
  */
 void print_buffer(char *b, int size)
 {
 	int i, j;
 
-	if (size <= 0)
-	{
-		printf("\n");
-		return;
-	}
-
 	for (i = 0; i < size; i += 10)
 	{
 		printf("%08x: ", i);
 
-		for (j = i; j < i + 10; j++)
+		for (j = 0; j < 10; j++)
 		{
-			if (j < size)
-				printf("%02x", b[j]);
+			if (i + j < size)
+				printf("%02x", (unsigned char)b[i + j]);
 			else
 				printf("  ");
 
-			if (j % 2 == 1)
+			if (j % 2 != 0)
 				printf(" ");
 		}
 
-		printf(" ");
-		for (j = i; j < i + 10; j++)
+		for (j = 0; j < 10; j++)
 		{
-			if (j < size)
+			if (i + j < size)
 			{
-				if (b[j] >= 32 && b[j] <= 126)
-					printf("%c", b[j]);
+				if (b[i + j] >= 32 && b[i + j] <= 126)
+					printf("%c", b[i + j]);
 				else
 					printf(".");
 			}
+			else
+				break;
 		}
+
 		printf("\n");
 	}
+
+	if (size <= 0)
+		printf("\n");
 }
