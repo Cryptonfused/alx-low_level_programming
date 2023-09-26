37ed52fe69149394a704eda7296d6cc515ee9060
Cryptonfused <cryptonfused@gmail.com>
2023-09-25 18:20:45 -0700

    trial fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i, m;
-	
-	m = n;
-	for (i = 0; i < m; i++)
+	int i;
+
+	for (i = 0; n > 0; i++)
 	{
 		*(s + i) = b;
+		n--;
 	}
 	return (s);
 }
