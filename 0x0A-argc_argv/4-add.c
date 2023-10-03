644bdd223b98bdeb5d5d203cf3002cf2a06410a8
Cryptonfused <cryptonfused@gmail.com>
2023-10-03 02:59:59 -0700

    code for program that adds a positive number

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,30 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 /**
- * main - Adds positive numbers and prints the result.
- * @argc: Number of command-line arguments.
- * @argv: Array of command-line argument strings.
+ * check_num - Check if a string contains only digits.
+ * @str: The input string.
  *
- * Return: 0 on success, 1 on error.
+ * Return: 1 if the string contains only digits, 0 otherwise.
+ */
+int check_num(char *str)
+{
+	unsigned int count;
+	unsigned int length = strlen(str);
+
+	count = 0;
+	while (count < length)
+	{
+		if (!isdigit(str[count]))
+			return (0);
+		count++;
+	}
+	return (1);
+}
+/**
+ * main - Entry point of the program.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 if successful, 1 if there is an error.
  */
 int main(int argc, char *argv[])
 {
-	int i, num, sum = 0;
+	int count;
+	int str_to_int;
+	int sum = 0;
 
-	for (i = 1; i < argc; i++)
+	count = 1;
+	while (count < argc)
 	{
-		if (*argv[i] == '-')
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]);
+			sum = sum + str_to_int;
+		}
+		else
 		{
 			printf("Error\n");
 			return (1);
 		}
-		num = atoi(argv[i]);
-		if (num < 0 || (num == 0 && *argv[i] != '0'))
-		{
-			printf("Error\n");
-			return (1);
-		}
-		sum += num;
+		count++;
 	}
 	printf("%d\n", sum);
 	return (0);
